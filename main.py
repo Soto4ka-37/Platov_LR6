b3ea84bf4b17f850b168084afdbf2416bb58ea8c
soto4ka37 <ars.platov@gmail.com>
2026-02-28 08:41:28 +0300

    Задача 3

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def task_2():
         
         if command[0] == 'push':
             if len(command) < 2:
-                print('Неверный ввод')
+                print('error')
                 continue
             queue.append(int(command[1]))
             print('ok')
@@ -45,13 +45,48 @@ def task_2():
         elif command[0] == 'exit':
             print('bye')
             break
-        
+
+def task_3():
+    parts = input("Введите коэффициенты -> ").split()
+    coefficients = list(map(float, parts))
+    roots = []
+    
+    if len(coefficients) == 1:
+        c = coefficients[0]
+        if c == 0:
+            roots = []
+    
+    elif len(coefficients) == 2:
+        b, c = coefficients
+        if b != 0:
+            roots = [-c / b]
+    
+    elif len(coefficients) == 3:
+        a, b, c = coefficients
+        if a == 0:
+            if b != 0:
+                roots = [-c / b]
+        else:
+            D = b * b - 4 * a * c
+            if D == 0:
+                roots = [-b / (2 * a)]
+            elif D > 0:
+                x1 = (-b - D ** 0.5) / (2 * a)
+                x2 = (-b + D ** 0.5) / (2 * a)
+                roots = [x1, x2]
+    
+    roots.sort()
+    print(*roots)
+
 while True:
     print(
         (
             "0 - Выход\n"
             "1 - Задача 1\n"
-            "2 - Задача 2"
+            "2 - Задача 2\n"
+            "3 - Задача 3\n"
+            "4 - Задача 4\n"
+
         )
     )
     n = input("Введите номер задачи -> ")
@@ -62,5 +97,7 @@ while True:
         task_1()
     elif n == "2":
         task_2()
+    elif n == "3":
+        task_3()
     else:
         print("Нет такой задачи")
