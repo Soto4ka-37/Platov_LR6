707d9ede096719cf3d370833ef25291e3329f5a4
soto4ka37 <ars.platov@gmail.com>
2026-02-28 08:23:23 +0300

    Добавил функцию для первой задачи

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 
+def task_1():
+    n = int(input("Введите количество городов -> "))
+    prices = list(map(int, input("Введите цены через пробел -> ").split()))
+    
+    for i in range(n):
+        found = -1
+        for j in range(i + 1, n):
+            if prices[j] < prices[i]:
+                found = j
+                break
+        print(found)
+
 while True:
     n = input()
     
